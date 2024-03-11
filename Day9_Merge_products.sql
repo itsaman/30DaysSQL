4d0fba8c3677b005c6b50ec0323b83cb7fe5dd12
Aman gupta <amang4996@gmail.com>
2024-03-11 23:07:09 +0530

    Create Day9_Merge_products.sql

---
+++ Day9_Merge_products.sql
@@ -0,0 +1,24 @@
+drop TABLE if exists orders;
+CREATE TABLE orders 
+(
+	customer_id 	INT,
+	dates 			DATE,
+	product_id 		INT
+);
+INSERT INTO orders VALUES
+(1, '2024-02-18', 101),
+(1, '2024-02-18', 102),
+(1, '2024-02-19', 101),
+(1, '2024-02-19', 103),
+(2, '2024-02-18', 104),
+(2, '2024-02-18', 105),
+(2, '2024-02-19', 101),
+(2, '2024-02-19', 106); 
+
+
+select customer_id, dates, to_varchar(product_id) from orders 
+union all
+select customer_id, dates, LISTAGG(product_id,',') as product_id 
+from orders
+group by customer_id, dates
+order by dates, customer_id
