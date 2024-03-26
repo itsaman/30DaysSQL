d9b9249e291eac8e65bde5806f1d8def48ce78ff
Aman gupta <amang4996@gmail.com>
2024-03-26 22:02:03 +0530

    Create Day25_Product_demo.sql

---
+++ Day25_Product_demo.sql
@@ -0,0 +1,20 @@
+drop table if exists product_demo;
+create table product_demo
+(
+	store_id	int,
+	product_1	varchar(50),
+	product_2	varchar(50)
+);
+insert into product_demo values (1, 'Apple - IPhone', '   Apple - MacBook Pro');
+insert into product_demo values (1, 'Apple - AirPods', 'Samsung - Galaxy Phone');
+insert into product_demo values (2, 'Apple_IPhone', 'Apple: Phone');
+insert into product_demo values (2, 'Google Pixel', ' apple: Laptop');
+insert into product_demo values (2, 'Sony: Camera', 'Apple Vision Pro');
+insert into product_demo values (3, 'samsung - Galaxy Phone', 'mapple MacBook Pro');
+
+
+select store_id,
+    sum(case when ltrim(lower(product_1)) like 'apple%' then 1 else 0 end) as pr1,
+    sum(case when ltrim(lower(product_2)) like 'apple%' then 1 else 0 end) as pr2
+from product_demo
+group by store_id
